205bdcb0578e260f4b5a383639860626fc0d4758
Diana Paliy <paliy.diana@gmail.com>
2015-03-17 14:44:27 +0300

    the first commit with empty files

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+    // Конфигурация проекта
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        //------------------------------------------------------------
+        less: { // Task less
+            options: {
+                expand: true
+            },
+            dev: { // Target
+                options: {
+                    strictMath: true
+                },
+                files: {
+                    'css/all.css': ['less/all.less']
+                }
+            },
+            release: { // Target
+                options: {
+                    strictMath: true,
+                    yuicompress: true
+                },
+                files: {
+                    'css/all.css': ['less/all.less']
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: 'less/**',
+                tasks: ['less:dev'],
+                options: {
+                    interrupt: true
+                }
+            },
+            css: {
+                files: ['*.css']
+            },
+            livereload: {
+                options: { livereload: true },
+                files: ['css/*.css']
+            }
+        }
+        //------------------------------------------------------------
+    });
+    // Инициализация плагинов, таски которых мы вызываем
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
